525c4ba34f1a04f456316b8ee7f7226db38b042d
Swapnil Gamit <gamitswapnil7@gmail.com>
2025-02-23 23:34:47 +0530

    added links/navbar

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,16 @@ const productModel=require('../models/prodct-model')
 
 router.get('/',(req,res)=>{
     let error =req.flash('error')
-    res.render('index',{error});
+    res.render('index',{error,loggedin:false});
 })
 router.get('/shop',isLoggedIn,async (req,res)=>{
     let products = await productModel.find();
     res.render('shop',{products});
 });
 
+router.get('/logout',isLoggedIn,(req,res)=>{
+     res.render('shop');
+})
 
 module.exports = router;
 
